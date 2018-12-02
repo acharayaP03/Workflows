22d95af7b2c557155ebd1714902225e2915c03c4
Prabhakar Acharya <acharyaP03@gmail.com>
2018-12-02 14:02:32 +1100

    add compass for sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -10,11 +11,14 @@ var jsSources = ['components/scripts/rclick.js',
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 //telling gulp task to write in the following files
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
-        .pipe(coffee({ bare: true })
+        .pipe(coffee({
+                bare: true
+            })
             .on('error', gutil.log)).pipe(gulp.dest('components/scripts'))
 });
 
@@ -23,4 +27,14 @@ gulp.task('js', function() {
         .pipe(concat('scripts.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+                sass: 'components/sass',
+                image: 'builds/development/images',
+                style: 'expanded'
+            })
+            .on('error', gutil.log)).pipe(gulp.dest('builds/development/css'))
 });
